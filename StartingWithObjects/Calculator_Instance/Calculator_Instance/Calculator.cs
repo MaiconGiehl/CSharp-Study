c5c9f852591df7f0b19d4532a116d52cb5a95e6b
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-19 14:21:50 -0300

    working-with-StaticMembers

---
+++ StartingWithObjects/Calculator_Instance/Calculator_Instance/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculator_Instance
+{
+    class Calculator
+    {
+        public static double Pi = 3.14;
+
+
+        public static double Circunference(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+
+    }
+}
---
+++ StartingWithObjects/Calculator_Instance/Calculator_Instance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculator_Instance
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius value: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculator.Circunference(radius);
+            double volume = Calculator.Volume(radius);
+
+            Console.WriteLine("Circunference: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
