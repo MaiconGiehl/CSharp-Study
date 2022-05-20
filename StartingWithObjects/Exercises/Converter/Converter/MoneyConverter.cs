6f1258ac8d7bc1e7b1ffc4f0cffe5789e3b593b9
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-20 13:41:45 -0300

    Converter

---
+++ StartingWithObjects/Exercises/Converter/Converter/MoneyConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Converter
+{
+    class MoneyConverter
+    {
+
+        public static double IoF = 1.1;
+        static public double TaxedValue(double Dollars, double ExchangeRate)
+        {
+            double finalValue = Dollars * ExchangeRate;
+            return finalValue + finalValue * IoF / 100.0;
+        }
+       
+    }
+}
---
+++ StartingWithObjects/Exercises/Converter/Converter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's the dollar exchange rate? ");
+            double exchangeRate = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("How many dollars do you want to buy? ");
+            double dollars = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double value = MoneyConverter.TaxedValue(exchangeRate, dollars);
+
+            Console.WriteLine($"You must pay R${value.ToString("F2", CultureInfo.InvariantCulture)}" +
+                $" for yours ${dollars.ToString("F2", CultureInfo.InvariantCulture)}.");
+        }
+    }
+}
