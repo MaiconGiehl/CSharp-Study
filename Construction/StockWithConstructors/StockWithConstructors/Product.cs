3340bbbbb73959a3cece9754de64442f43f9674c
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-20 14:17:50 -0300

    starting-with-constructors

---
+++ Construction/StockWithConstructors/StockWithConstructors/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Constructors
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Amount;
+
+        // Construcao padrao
+        public Product() { }
+        
+        public Product(string name, double price, int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+            Amount = 5;
+        }
+
+        public double TotalInStock()
+        {
+            return Price * Amount;
+        }
+
+        public void AdcProducts(int moreProducts)
+        {
+            Amount += moreProducts;
+        }
+
+        public void RmvProducts(int lessProducts)
+        {
+            Amount -= lessProducts;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Amount
+                + " units, Total: $"
+                + TotalInStock().ToString("F2", CultureInfo.InvariantCulture)
+                + ".";
+        }
+    }
+}
---
+++ Construction/StockWithConstructors/StockWithConstructors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insert the product data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Price: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Product p = new Product(name, price);
+
+            Product p2 = new Product();
+
+            Console.WriteLine();
+            Console.WriteLine("Product data: " + p);
+            Console.WriteLine();
+
+
+            Console.Write("Type how many products are going to be inserted: ");
+            int moreProducts = int.Parse(Console.ReadLine());
+            p.AdcProducts(moreProducts);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated product data: " + p);
+            Console.WriteLine();
+
+            Console.Write("Type how many products are going to be taken: ");
+            int lessProducts = int.Parse(Console.ReadLine());
+            p.RmvProducts(lessProducts);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated product data: " + p);
+
+        }
+    }
+}
