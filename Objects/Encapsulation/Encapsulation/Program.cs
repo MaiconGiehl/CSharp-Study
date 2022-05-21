be6cbd2777722e260177823301a4ba2afb57d134
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-21 14:33:15 -0300

    organizing

---
+++ Objects/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product("TV", 500.00, 10); ;
+
+            p.SetName("TV 4K");
+            p.SetName("A");
+            // Não funciona por causa da condicao
+            // Encapsulamento melhora seu controle sobre as entradas e saídas
+
+
+            Console.WriteLine(p.GetName());
+            Console.WriteLine(p.GetPrice());
+            Console.WriteLine(p.GetAmount());
+
+
+        }
+    }
+}
