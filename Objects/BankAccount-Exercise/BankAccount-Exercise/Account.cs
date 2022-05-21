21ff5dfac6f0ae0248d33a10f78224b2fe0bd9a8
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-21 15:15:03 -0300

    bank-correction

---
+++ Objects/BankAccount-Exercise/BankAccount-Exercise/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankAccount
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Owner { get; set; }
+        public double Balance { get; private set; }
+
+
+        public Account(int number, string owner)
+        {
+            Number = number;
+            Owner = owner;
+            Balance = 0;
+
+        }
+        public Account(int number, string owner, double firstDeposit) : this(number, owner)
+        {
+            Deposit(firstDeposit);
+        }
+
+        public void Deposit (double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw (double amount)
+        {
+            Balance -= amount;
+            Balance -= 5.0;
+        }
+        
+        public override string ToString()
+        {
+            return $"Account {Number}, " +
+                $"Owner {Owner}, " +
+                $"Balance ${Balance.ToString("F2", CultureInfo.InvariantCulture)}.";
+        }
+    }
+}
---
+++ Objects/BankAccount-Exercise/BankAccount-Exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account account;
+
+            Console.Write("Enter the account number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the account owner's name: ");
+            string owner = Console.ReadLine();
+
+            Console.Write("Do you want to put money in your account (y/n)? ");
+            char answer = char.Parse(Console.ReadLine());
+
+            if(answer == 'y' || answer == 'Y')
+            {
+                Console.Write("Enter the deposit amount: ");
+                double firstDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new Account(number, owner, firstDeposit);
+            } else
+            {
+                account = new Account(number, owner);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Account information: " + account);
+            Console.WriteLine("");
+
+            Console.Write("Enter a deposit amount: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(amount);
+
+            Console.WriteLine("");
+            Console.WriteLine("Updated account information: " + account);
+            Console.WriteLine("");
+
+            Console.Write("Enter a value to withdraw: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Withdraw(amount);
+            
+            Console.WriteLine("");
+            Console.WriteLine("Updated account information: " + account);
+        }
+    }
+}
