f61eb776dd682d0c65e5997a1c424f0f60163479
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-22 17:18:03 -0300

    vector-theory-and-exercise

---
+++ Memory/Vectors/VectorExercise/VectorExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int totalRent = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Register[] registers = new Register[10];
+
+            for (int i = 1; i <= totalRent; i++)
+            {
+                Console.WriteLine($"Rent #" + i);
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                registers[room] = new Register { Rent = i, Name = name, Email = email, Room = room };
+
+            }
+
+            Console.WriteLine("Busy rooms");
+
+            for (int i = 0; i < registers.Length; i++)
+
+                if (registers[i] != null)
+                {
+                    Console.WriteLine(registers[i]);
+                }
+        }
+
+    }
+}
---
+++ Memory/Vectors/VectorExercise/VectorExercise/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise
+{
+    internal class Register
+    {
+        public int Rent;
+        public string Name;
+        public string Email;
+        public int Room;
+
+        public override string ToString()
+        {
+            return $"{Room}: {Name}, {Email}";
+        }
+    }
+}
