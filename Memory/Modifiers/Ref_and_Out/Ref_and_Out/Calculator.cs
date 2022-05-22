855662e240db0708cd7a9240ac8733df74af9954
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-22 20:22:40 -0300

    modifiers

---
+++ Memory/Modifiers/Ref_and_Out/Ref_and_Out/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Params
+{
+    internal class Calculator
+    {
+        public static void TripleWithRef(ref int a)
+        {
+            a = a * 3;
+        }
+
+        public static void TripleWithOut(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Memory/Modifiers/Ref_and_Out/Ref_and_Out/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ref
+            int x = 10;
+            Calculator.TripleWithRef(ref x);
+            Console.WriteLine(x);
+
+            // out
+            int y = 10;
+            int triple;
+            Calculator.TripleWithOut(y, out triple);
+            Console.WriteLine(triple);
+
+            // OBS: esses modificadores são considerados "código ruim"
+
+        }
+    }
+}
