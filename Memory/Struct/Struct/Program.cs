bdb620a62389fe249e7ec2189618acee238335ec
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-22 15:35:19 -0300

    memory-behavior

---
+++ Memory/Struct/Struct/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p; // necessário inicializar, mas não precisa do "Point p = new Point()"
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
