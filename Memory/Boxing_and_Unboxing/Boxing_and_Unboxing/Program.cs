f07b4e8ce5386b5df561f84b7eef61cab7f8855b
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-22 22:47:32 -0300

    working-with-lists

---
+++ Memory/Boxing_and_Unboxing/Boxing_and_Unboxing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boxing = É o processo de conversão de um objeto tipo valor para um objeto tipo referência compatível
+
+            int x = 20;
+
+            Object objBx = x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(objBx);
+
+
+
+            // Unboxing = é o processo de conversão de um objeto tipo valor compatível
+
+            int y = 20;
+            Object objUbx = y;
+
+
+            Console.WriteLine(y);
+            Console.WriteLine(objUbx);
+
+            int z = (int)objUbx;
+
+            Console.WriteLine(z);
+        }
+    }
+}
---
+++ Memory/ForEach/ForEach/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sintaxe opcional e simplificada para percorrer coleções
+
+            string[] vect = new string[] { "Maria" , "Alex", "Bob"};
+
+
+            // Normal
+            for(int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+            // Foreach
+            foreach (string name in vect)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
