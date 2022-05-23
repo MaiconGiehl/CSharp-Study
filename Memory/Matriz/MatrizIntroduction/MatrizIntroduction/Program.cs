0fb692f06e7d39202baf70c8413979a3f3bb2442
maicongiehl@gmail.com <maicongiehl@gmail.com>
2022-05-23 18:02:16 -0300

    Matriz

---
+++ Memory/Matriz/MatrizIntroduction/MatrizIntroduction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);
+
+            Console.WriteLine(mat.Rank);
+
+            Console.WriteLine(mat.GetLength(0)); // Número de linhas
+
+            Console.WriteLine(mat.GetLength(1)); // Número de colunas
+        }
+    }
+}
