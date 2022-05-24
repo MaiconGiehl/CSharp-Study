6b689c9372aa1dd97e27f9d056ee82072aa7011a
MaiconGiehl <maicongiehl@gmail.com>
2022-05-24 17:16:20 -0300

    C#-special-topics

---
+++ SpecialTopics/Var/Var/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "John";
+
+            //var a = z;
+            //function (a) {
+            //  a + 2;
+            //}
+            // Preferível declarar o tipo do dado, pois obriga
+            // o programador a acertar.
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
