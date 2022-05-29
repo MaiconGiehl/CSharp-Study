f8e9a9f5c4d1840ebd9161f6cf158fd45ef6eaa1
Maicon Giehl <maicongiehl@gmail.com>
2022-05-28 22:58:22 -0300

    working-with-enumeration

---
+++ Enumeration/EnumerationExercise01/EnumerationExercise01/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace EnumerationExercise01.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2,
+    }
+
+
+}
---
+++ Enumeration/EnumerationExercise01/EnumerationExercise01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnumerationExercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                
+        }
+    }
+}
