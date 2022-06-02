790aefcd9865cf5591bea721a5ffcca396a15427
Maicon Giehl <maicongiehl@gmail.com>
2022-06-02 12:30:48 -0300

    CustomExceptions-Part01

---
+++ ExceptionTreatment/CustomExceptions/CustomExceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Course.Entities;
+public class ProcessFIle
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Room number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Check-in date (dd/m//yyyy): ");
+        DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Check-out date (dd/m//yyyy): ");
+        DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+        if (checkOut <= checkIn)
+        {
+            Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+        } 
+        else
+        {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.Write("Check-in date (dd/m//yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-out date (dd/m//yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+            }
+            else if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check out data must be after check-in date");
+            }
+            else
+            {
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+        }
+    }
+}
