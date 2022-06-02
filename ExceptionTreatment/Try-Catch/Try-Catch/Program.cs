1516d2a25edcd187c4c4576715bbb9e3a3eeed15
Maicon Giehl <maicongiehl@gmail.com>
2022-06-02 11:52:08 -0300

    try-catch

---
+++ ExceptionTreatment/Try-Catch/Try-Catch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+            }
+            catch (DivideByZeroException e)
+            {
+                //Console.WriteLine("Error! " + e.Message);
+                Console.WriteLine("Division by zero is not allowed");
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
+        }
+    }
+}
