89d2085a89cea1551d20049e2c325f353288f6ed
Maicon Giehl <maicongiehl@gmail.com>
2022-06-02 20:20:49 -0300

    ExceptionTreatment-Exercise

---
+++ ExceptionTreatment/ExceptionTreatmentExercise/ExceptionTreatmentExercise/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exercise.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ ExceptionTreatment/ExceptionTreatmentExercise/ExceptionTreatmentExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Exercise.Entities;
+using Exercise.Entities.Exceptions;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account account = new Account(number, holder, initBalance, withdrawLimit);
+
+                Console.Write("\r\nEnter amount for withdraw: ");
+                account.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                Console.Write($"New Balance: ${account}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
