6598aa9d9ab898b7d22e7bae0bf556ba28727259
Maicon Giehl <maicongiehl@gmail.com>
2022-06-02 13:20:49 -0300

    CustomExceptions-Part02

---
+++ ExceptionTreatment/CustomExceptions/BadCustomException/BadCustomException/Entities/Reservation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Reservation dates for update must be future dates";
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Check out data must be after check-in date";
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Room: {RoomNumber}, " +
+                $"Check-In: {CheckIn.ToString("dd/MM/yyyy")}, " +
+                $"Check-Out {CheckOut.ToString("dd/MM/yyyy")}, " +
+                $"{Duration()} nights";
+        }
+
+    }
+}
---
+++ ExceptionTreatment/CustomExceptions/BadCustomException/BadCustomException/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Course.Entities;
+public class ProcessFIle
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Room number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Check-in date (dd/m//yyyy): ");
+        DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Check-out date (dd/m//yyyy): ");
+        DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+        if (checkOut <= checkIn)
+        {
+            Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+        } 
+        else
+        {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.Write("Check-in date (dd/m//yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-out date (dd/m//yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            string error = reservation.UpdateDates(checkIn, checkOut);
+            if (error != null)
+            {
+                Console.WriteLine("Error in reservation: " + error);
+            }
+            else
+            {
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            
+        }
+    }
+}
