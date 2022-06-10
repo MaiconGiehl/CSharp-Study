37ce4c6594a2b525ca4ac800051eeabb9bb7f672
Maicon Giehl <maicongiehl@gmail.com>
2022-06-09 23:31:19 -0300

    starting-with-file-manipulation

---
+++ Working-with-Files/C#/FileManipulation-Introduction/FileManipulation-Introduction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FileManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"YourPathTill\01\file1.txt";
+
+            string targetPath = @"YourPathTill\01\file2.txt";
+
+            // creates a new file in 01's folder with the same content
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                // Read the file
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                // Tell you all the file lines
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+                // you can't create because it already exist
+            }
+
+        }
+    }
+}
