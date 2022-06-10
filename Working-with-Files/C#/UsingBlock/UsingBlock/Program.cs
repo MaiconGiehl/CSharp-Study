715f6a30a78089cfb84b49930b06f215ce7dedb3
Maicon Giehl <maicongiehl@gmail.com>
2022-06-10 00:03:12 -0300

    Block-Using

---
+++ Working-with-Files/C#/UsingBlock/UsingBlock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+
+namespace FileManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Maicon\Desktop\Udemy\CursoCSharp\Working-with-Files\Files-to-Acess\03\file1.txt";
+
+            try
+            {
+                //using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    //using (StreamReader sr = new StreamReader(fs))
+                    using (StreamReader sr = File.OpenText(path)) // Simpler
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
