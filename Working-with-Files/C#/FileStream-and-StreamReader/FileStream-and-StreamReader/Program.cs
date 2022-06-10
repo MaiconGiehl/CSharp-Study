013f9d6bb8d2e182c6c6ceb1a453514599f0a6f5
Maicon Giehl <maicongiehl@gmail.com>
2022-06-09 23:48:57 -0300

    fs-and-sr

---
+++ Working-with-Files/C#/FileStream-and-StreamReader/FileStream-and-StreamReader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+
+namespace FileManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"YourPathTill\02\file1.txt";
+
+            //FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                // fs = new FileStream(path, FileMode.Open);
+                //sr = new StreamReader(fs);
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                //if (fs != null) fs.Close();
+            }
+        }
+    }
+}
