044b8064e9ce0678e841548c3be0ece6e162da50
Maicon Giehl <maicongiehl@gmail.com>
2022-06-11 19:26:31 -0300

    FileManipulation-Exercise

---
+++ Working-with-Files/C#/FileManipulation-Exercise/FileManipulation-Exercise/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace FileManipulation_Exercise.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+
+        public Product(string name, double price, int qty)
+        {
+            Name = name;
+            Price = price;
+            Qty = qty;
+        }
+
+        public double TotalPrice ()
+        {
+            return Price * Qty;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name},{TotalPrice().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ Working-with-Files/C#/FileManipulation-Exercise/FileManipulation-Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using FileManipulation_Exercise.Entities;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert the file path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = @$"{sourceFolderPath}\Out";
+                string targetFilePath = @$"{targetFolderPath}\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] properties = line.Split(',');
+
+                        string name = properties[0];
+                        double price = double.Parse(properties[1], CultureInfo.InvariantCulture);
+                        int qty = int.Parse(properties[2]);
+                        Product product = new Product(name, price, qty);
+
+                        sw.WriteLine(product);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
