559f88b51bb7b90b7151dfc475b40feef3d9d7e7
Maicon Giehl <maicongiehl@gmail.com>
2022-06-26 20:36:11 -0300

    generic-implementation-done

--- Generics/Generics/Generics/Program.cs
+++ Generics/Generics/Generics/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How many values? ");
 
@@ -15,6 +15,11 @@
                 int x = int.Parse(Console.ReadLine());
                 printService.AddValue(x);
             }
+
+            int a = printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
+
 
             printService.Print();
             Console.WriteLine("First: " + printService.First());
