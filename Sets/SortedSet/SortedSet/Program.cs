a77e632f7242e26b63ee1d636ab119774f3b489b
Maicon Giehl <maicongiehl@gmail.com>
2022-06-27 16:30:59 -0300

    sorted-set

---
+++ Sets/SortedSet/SortedSet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> a = new SortedSet<int>() { 10, 0, 2, 4, 5, 6, 8,  };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            // Union
+            Console.WriteLine("Union");
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            // Intersection
+            Console.WriteLine("Intersection");
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            // difference
+            Console.WriteLine("Difference");
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+            
+        }
+
+        static void PrintCollection<T> (IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine("\r\n================");
+        }
+    }
+}
