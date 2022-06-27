12b533a878fbcf0f382050b28e56c9126bc89eb4
Maicon Giehl <maicongiehl@gmail.com>
2022-06-27 15:52:36 -0300

    hashset-and-equals

---
+++ Generics/Restrictions/GetHashCode-and-Equals/GetHashCode-and-Equals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Set.Entities;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+            Client c = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.Equals(c));
+
+            Console.WriteLine(a == b);
+            Console.WriteLine(a == c);
+            Console.WriteLine(a.Email == c.Email);
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            Console.WriteLine(c.GetHashCode());
+        }
+    }
+}
