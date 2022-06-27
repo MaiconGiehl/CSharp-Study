1068dca7827dae7cf6dd0801a0852195b201d36b
Maicon Giehl <maicongiehl@gmail.com>
2022-06-27 16:18:13 -0300

    starting-with-sets

---
+++ Sets/HashSet/HashSet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Set
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+
+            Console.WriteLine(set.Contains("Notebook"));
+            Console.WriteLine(set.Contains("Microwave"));
+
+            Console.WriteLine("---------");
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
