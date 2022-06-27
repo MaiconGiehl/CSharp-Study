7acae147855e33421defbc0fb7f2aea3f972c952
Maicon Giehl <maicongiehl@gmail.com>
2022-06-27 16:57:37 -0300

    Sets-Comparasion

---
+++ Sets/EqualityComparison/EqualityComparison/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Set.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (!(obj is Product))
+            {
+                return false;
+            }
+
+            Product other = obj as Product;
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
