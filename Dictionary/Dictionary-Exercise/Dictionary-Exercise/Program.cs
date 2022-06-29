a74dd9e993d35f13947ca7cb4f0c21ebc9003f39
Maicon Giehl <maicongiehl@gmail.com>
2022-06-28 23:22:37 -0300

    dictionary-exercise

---
+++ Dictionary/Dictionary-Exercise/Dictionary-Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            Dictionary<string, int> results = new Dictionary<string, int>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] ballot = sr.ReadLine().Split(',');
+                        string name = ballot[0];
+                        int votes = int.Parse(ballot[1]);
+
+                        if (results.ContainsKey(name))
+                        {
+                            results[name] = results[name] + votes;
+                        }
+                        else
+                        {
+                            results[name] = votes;
+                        }
+                    }
+                    foreach (KeyValuePair<string, int> result in results)
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+            catch (IOException error)
+            {
+                Console.WriteLine("Error:");
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
