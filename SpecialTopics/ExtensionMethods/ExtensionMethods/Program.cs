ec9d221bdae3a19b14fe051db0193e7c66e6b7a2
Maicon Giehl <maicongiehl@gmail.com>
2022-06-30 16:43:27 -0300

    method-extension

---
+++ SpecialTopics/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtMeth
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 05, 30, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
