693881fe320eecf83b44cabf4e5f3aad6282f1f8
Maicon Giehl <maicongiehl@gmail.com>
2022-07-06 15:26:33 -0300

    simulating-sql

--- Linq/LinqWithLambda/LinqWithLambda/Program.cs
+++ Linq/LinqWithLambda/LinqWithLambda/Program.cs
@@ -36,19 +36,49 @@ namespace LinqWithLambda
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
             Print("TIER 1 AND PRICE < 900:", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
             Print("NAMES OF PRODUCTS FROM TOOLS", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, Categoryname = p.Category.Name});
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, Categoryname = p.Category.Name});
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
             Print("TIER 1 ORDER BY PRICE AND THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p)
+                 .Skip(2)
+                 .Take(4);
             Print("TIER 1 ORDER BY PRICE AND THEN BY NAME SKIP 2 TAKE 4", r5);
 
             //var r6 = products.First();
@@ -56,9 +86,14 @@ namespace LinqWithLambda
             //var r7 = products.Where(p => p.Price > 3000.0).First();
             //Console.WriteLine.First();
 
-            var r6 = products.FirstOrDefault();
+            //var r6 = products.FirstOrDefault();
+            var r6 = (from p in products select p).FirstOrDefault();
             Console.WriteLine("Firt or Default test1: " + r6);
-            var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            var r7 = 
+                (from p in products
+                where p.Price > 3000.0
+                select p).FirstOrDefault();
             Console.WriteLine("First or Default test2: " + r7);
             
             Console.WriteLine();
@@ -86,7 +121,10 @@ namespace LinqWithLambda
             Console.WriteLine("Category 1 aggregate sum (Handling error): " + r15_2);
 
             Console.WriteLine("\r\n");
-            var r16 = products.GroupBy(p => p.Category);
+
+            var r16 =
+                from p in products
+                group p by p.Category;
             foreach (IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
@@ -94,6 +132,7 @@ namespace LinqWithLambda
                 {
                     Console.WriteLine(p);
                 }
+                Console.WriteLine();
             }
 
         }
